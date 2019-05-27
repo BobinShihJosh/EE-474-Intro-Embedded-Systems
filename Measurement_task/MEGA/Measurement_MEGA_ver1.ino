d920fc38b9eca34b99072c4e97b4a3c09ddb7f60
glacierdidi <49403334+glacierdidi@users.noreply.github.com>
2019-05-27 07:28:19 -0700

    Add files via upload

---
+++ Measurement_task/MEGA/Measurement_MEGA_ver1.ino
@@ -0,0 +1,33 @@
+#include "MeasureSubsystem.h"
+#include "circularBuffer.h"
+
+unsigned int temperatureRaw;
+unsigned int bloodPressRaw;
+unsigned int pulseRateRaw;
+unsigned int respirationRateRaw;
+unsigned short measurementSelection =1;
+
+measureSubsystemData measuredata;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  init_all();
+}
+
+void loop()
+{
+  MeasureSubsystemFunction(&measuredata);
+  delay(1000);
+}
+
+void init_all()
+{
+  measuredata.temperatureRawPtr=&temperatureRaw;
+  measuredata.bloodPressRawPtr=&bloodPressRaw;
+  measuredata.pulseRateRawPtr=&pulseRateRaw;
+  measuredata.respirationRateRawPtr=&respirationRateRaw;
+  measuredata.measurementSelectionPtr=&measurementSelection;
+
+}
